0f069cebfe362b028d1d5a351958fea948d67db8
Mohamed Elshowel <mohamed.elshowel@gmail.com>
2021-08-18 07:52:43 +0200

    feat: Add new component to handle miss-spelled routes

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function NotFound() {
+  return <div className="center">
+      <h2>⚠️ Error 404!</h2>
+      <span>The requested pages is not found</span>
+  </div>;
+}
+
+export default NotFound;
